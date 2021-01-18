6bd9cacb69ec8b562fd5c7764d064e20a358bd1e
abu-sayem <abusaayem@gmail.com>
2021-01-18 23:32:49 +0600

    added basic user app

--- pizza_backend/pizza_backend/settings.py
+++ pizza_backend/pizza_backend/settings.py
@@ -37,7 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+
+    'users.apps.UsersConfig',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
