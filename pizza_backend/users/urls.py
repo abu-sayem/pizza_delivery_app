f2802a2c5d5839573784a1513de461038e6e98d7
abu-sayem <abusaayem@gmail.com>
2021-01-22 09:29:29 +0600

    changed django-rest auth to dj-rest-auth

--- pizza_backend/users/urls.py
+++ pizza_backend/users/urls.py
@@ -1,10 +1,14 @@
 
 from django.urls import include, path
 from . import views
+from rest_framework_simplejwt import views as jwt_views
+
 
 urlpatterns = [
 
-    #path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('sign_up/', include('dj_rest_auth.registration.urls')),
     #path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('', views.UserListView.as_view()),
 ]
