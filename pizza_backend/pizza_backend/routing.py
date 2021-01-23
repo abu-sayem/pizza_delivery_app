cb70e55ce04de6ffffec719d80dac36389a0b7c9
abu-sayem <abusaayem@gmail.com>
2021-01-23 07:56:20 +0600

    added routing to get pizza consumer

--- pizza_backend/pizza_backend/routing.py
+++ pizza_backend/pizza_backend/routing.py
@@ -1,3 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from django.urls import path 
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-application = ProtocolTypeRouter({})
+from pizzas.consumers import PizzaConsumer
+
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([
+        path('pizza/', PizzaConsumer),
+    ]),
+})
