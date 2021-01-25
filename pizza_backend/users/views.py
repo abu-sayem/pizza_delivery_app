cacecf3eae638553daa996347396d370c72eb02a
abu-sayem <abusaayem@gmail.com>
2021-01-25 08:58:09 +0600

    added UserViewSet

--- pizza_backend/users/views.py
+++ pizza_backend/users/views.py
@@ -1,9 +1,25 @@
 # users/views.py
 from rest_framework import generics
+from django.db.models.query import QuerySet
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from rest_framework import viewsets, permissions
+
+from .serializers import UserSerializer
 
 from . import models
 from . import serializers
 
-class UserListView(generics.ListAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
