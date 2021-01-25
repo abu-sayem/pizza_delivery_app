4a351649d9468dbee4c9b5ac5a7c1fe551bd094d
abu-sayem <abusaayem@gmail.com>
2021-01-25 09:02:51 +0600

    added TokenAuthMiddlewareStack in pizza routing for auth purpose

--- pizza_backend/pizza_backend/routing.py
+++ pizza_backend/pizza_backend/routing.py
@@ -1,11 +1,12 @@
 from django.urls import path 
 from channels.routing import ProtocolTypeRouter, URLRouter
-
+from .middleware import TokenAuthMiddlewareStack 
 from pizzas.consumers import PizzaConsumer
 
 
 application = ProtocolTypeRouter({
-    'websocket': URLRouter([
+    'websocket': TokenAuthMiddlewareStack( URLRouter([
         path('pizza/', PizzaConsumer),
     ]),
+    ),
 })
