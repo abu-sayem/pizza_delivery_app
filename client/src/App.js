1466f3cdd4b544186bdea25599171d75daace8bf
abu-sayem <abusaayem@gmail.com>
2021-02-01 10:36:50 +0600

    added login and signup form without api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Route, Switch, NavLink, Link } from 'react-router-dom';
+import { Container, Navbar } from 'react-bootstrap'; 
+import { LinkContainer } from 'react-router-bootstrap';
+import SignUp from './features/users/SignUp'
+import LogIn from './features/users/LogIn'
+
 import './App.css';
 
 function App() {
   return (
-    <h1>Orders</h1>
+    <>
+      <Navbar bg='light' expand='lg' variant='light'>
+        <LinkContainer to='/'>
+          <Navbar.Brand className='logo'>Pizza</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse></Navbar.Collapse>
+      </Navbar>
+      <Container className='pt-3'></Container>
+    <Switch>
+      <Route exact path='/' render={() => (
+        <div className='middle-center'>
+          <h1 className='landing logo'>Pizza</h1>
+          <Link className='btn btn-primary' to='/sign-up'>Sign up</Link>
+          <Link className='btn btn-primary' to='/log-in'>Log in</Link>
+        </div>
+      )} />
+      <Route path='/sign-up' component={SignUp} />
+      <Route path='/log-in' component={LogIn} />
+    </Switch>
+    </>
   );
 }
 
